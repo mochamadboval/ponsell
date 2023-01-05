5c65867b0bb5cc1165956e6f76787fb11c9d736c
Mochamad Boval <mbovalap@gmail.com>
2023-01-06 03:30:39 +0700

    Add Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,36 @@
-import { Inter } from "@next/font/google";
+import { Inter, Libre_Barcode_39_Text } from "@next/font/google";
+import { Fragment } from "react";
 
 import MenuItem from "../components/MenuItem";
 
 import "../styles/globals.css";
 
+const libreBarcode39Text = Libre_Barcode_39_Text({
+  subsets: ["latin"],
+  weight: "400",
+});
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-      <nav className="bg-white bottom-0 fixed w-full">
-        <ul className="flex items-center justify-center">
-          <MenuItem name="home">Home</MenuItem>
-          <MenuItem name="categories">Categories</MenuItem>
-          <MenuItem name="cart">Cart</MenuItem>
-          <MenuItem name="wishlist">Wishlist</MenuItem>
-          <MenuItem name="account">Account</MenuItem>
-        </ul>
-      </nav>
-    </main>
+    <Fragment>
+      <header
+        className={`${libreBarcode39Text.className} bg-white border-b-2 border-white fixed shadow-sm py-1 top-0 w-full`}
+      >
+        <h1 className="text-4xl text-center">Ponsell</h1>
+      </header>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+        <nav className="bg-white bottom-0 fixed shadow-sm w-full">
+          <ul className="flex items-center justify-center">
+            <MenuItem name="home">Home</MenuItem>
+            <MenuItem name="categories">Categories</MenuItem>
+            <MenuItem name="cart">Cart</MenuItem>
+            <MenuItem name="wishlist">Wishlist</MenuItem>
+            <MenuItem name="account">Account</MenuItem>
+          </ul>
+        </nav>
+      </main>
+    </Fragment>
   );
 }
