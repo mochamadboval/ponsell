f15275f3ec3c5c8417e240473258287b7e56e168
Mochamad Boval <mbovalap@gmail.com>
2023-01-06 03:06:41 +0700

    Add Menu

---
+++ components/MenuItem.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function MenuItem(props) {
+  const router = useRouter();
+
+  const { children, name } = props;
+
+  const path = name === "home" ? "/" : `/${name}`;
+  const active = router.pathname === path ? "border-green-600" : "border-white";
+
+  return (
+    <li className="w-1/5">
+      <Link
+        href={path}
+        className={`border-t-2 flex flex-col items-center py-1 ${active}`}
+      >
+        <Image src={`/icons/${name}.svg`} alt="" width={24} height={24} />
+        <p className="text-xs">{children}</p>
+      </Link>
+    </li>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { Inter } from "@next/font/google";
 
+import MenuItem from "../components/MenuItem";
+
 import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,6 +10,15 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
+      <nav className="bg-white bottom-0 fixed w-full">
+        <ul className="flex items-center justify-center">
+          <MenuItem name="home">Home</MenuItem>
+          <MenuItem name="categories">Categories</MenuItem>
+          <MenuItem name="cart">Cart</MenuItem>
+          <MenuItem name="wishlist">Wishlist</MenuItem>
+          <MenuItem name="account">Account</MenuItem>
+        </ul>
+      </nav>
     </main>
   );
 }
