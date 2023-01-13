d954e776406db9bbb535748c7e0706d302c4ee61
Mochamad Boval <mbovalap@gmail.com>
2023-01-13 13:37:05 +0700

    Add account form metadata

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,4 +1,6 @@
-import { useRef, useState } from "react";
+import Head from "next/head";
+
+import { Fragment, useRef, useState } from "react";
 
 import FormInput from "../../components/UI/FormInput";
 
@@ -48,42 +50,54 @@ export default function Account() {
   };
 
   return (
-    <div className="max-w-screen-sm mx-auto px-4 py-5">
-      <article className="bg-white p-4 rounded-md shadow-sm">
-        <h2 className="font-bold text-base text-center">
-          {isLoginForm ? "Login" : "Sign Up"}
-        </h2>
-        {isInvalidInput && (
-          <p className="bg-red-200 mt-4 p-2 rounded-md text-center">
-            {invalidMessage}
-          </p>
-        )}
-        <form
-          className="flex flex-col gap-2 pt-4"
-          onSubmit={isLoginForm ? loginHandler : createNewUserHandler}
-        >
-          <FormInput label="email" name="Email" type="email" ref={emailRef} />
-          <FormInput
-            label="password"
-            name="Password"
-            type="password"
-            ref={passwordRef}
-          />
-          {!isLoginForm && (
-            <FormInput label="name" name="Name" type="text" ref={nameRef} />
+    <Fragment>
+      <Head>
+        <title>{`${isLoginForm ? "Login" : "Sign Up"} - Ponsell`}</title>
+        <meta
+          name="description"
+          content={`${isLoginForm ? "Login" : "Sign Up"} to Ponsell`}
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="max-w-screen-sm mx-auto px-4 py-5">
+        <article className="bg-white p-4 rounded-md shadow-sm">
+          <h2 className="font-bold text-base text-center">
+            {isLoginForm ? "Login" : "Sign Up"}
+          </h2>
+          {isInvalidInput && (
+            <p className="bg-red-200 mt-4 p-2 rounded-md text-center">
+              {invalidMessage}
+            </p>
           )}
-          <button className="bg-green-700 mt-2 py-3 rounded-md shadow-sm text-white">
-            {isLoginForm ? "Login" : "Create Account"}
+          <form
+            className="flex flex-col gap-2 pt-4"
+            onSubmit={isLoginForm ? loginHandler : createNewUserHandler}
+          >
+            <FormInput label="email" name="Email" type="email" ref={emailRef} />
+            <FormInput
+              label="password"
+              name="Password"
+              type="password"
+              ref={passwordRef}
+            />
+            {!isLoginForm && (
+              <FormInput label="name" name="Name" type="text" ref={nameRef} />
+            )}
+            <button className="bg-green-700 mt-2 py-3 rounded-md shadow-sm text-white">
+              {isLoginForm ? "Login" : "Create Account"}
+            </button>
+          </form>
+          <p className="pt-2 text-center">or</p>
+          <button
+            className="border border-green-700 mt-2 py-3 rounded-md w-full"
+            onClick={switchFormHandler}
+          >
+            {isLoginForm ? "Sign Up" : "Login"}
           </button>
-        </form>
-        <p className="pt-2 text-center">or</p>
-        <button
-          className="border border-green-700 mt-2 py-3 rounded-md w-full"
-          onClick={switchFormHandler}
-        >
-          {isLoginForm ? "Sign Up" : "Login"}
-        </button>
-      </article>
-    </div>
+        </article>
+      </div>
+    </Fragment>
   );
 }
