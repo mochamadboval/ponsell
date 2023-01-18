2d6df46739723f455e41357e9ac427d86a2e1afa
Mochamad Boval <mbovalap@gmail.com>
2023-01-18 16:32:52 +0700

    Show wishlist

--- pages/wishlist/index.js
+++ pages/wishlist/index.js
@@ -1,9 +1,42 @@
+import { firebaseURL, isUserExists } from "../api/auth/signup";
+
+import Head from "next/head";
 import { getSession } from "next-auth/react";
 
-export default function Wishlist(props) {
-  console.log(props.user);
+import { Fragment } from "react";
 
-  return <p>Wishlist</p>;
+import ProductItem from "../../components/Product/ProductItem";
+
+export default function Wishlist(props) {
+  const { products, user } = props;
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Wishlist - Ponsell</title>
+        <meta
+          name="description"
+          content={`Wishlist of ${user.name} on Ponsell.`}
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex flex-wrap gap-4 px-4 py-5">
+        {products.length !== 0 ? (
+          products.map((product, index) => (
+            <ProductItem
+              key={product.productId}
+              product={product}
+              index={index}
+            />
+          ))
+        ) : (
+          <p className="text-center w-full">No products.</p>
+        )}
+      </div>
+    </Fragment>
+  );
 }
 
 export async function getServerSideProps(context) {
@@ -18,7 +51,18 @@ export async function getServerSideProps(context) {
     };
   }
 
+  const user = await isUserExists(session.user.email);
+  const userId = user[Object.keys(user)[0]].id;
+
+  const response = await fetch(`${firebaseURL}/wishlist/${userId}.json`);
+  const data = await response.json();
+
+  const wishlisted = [];
+  for (const productKey in data) {
+    wishlisted.unshift(data[productKey]);
+  }
+
   return {
-    props: { user: session.user },
+    props: { products: wishlisted, user: session.user },
   };
 }
